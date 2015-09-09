ad5faaa11bd3e56927dcad223e20877ce5fc060c
Kevin Novak <kevinmnovak@yahoo.com>
2015-09-09 11:22:44 -0400

    Prompt for flashline and phone, creates folder

--- backup.bat
+++ backup.bat
@@ -1,1 +1,23 @@
-if not exist "C:\Users\kevin\Desktop\Test" mkdir C:\Users\kevin\Desktop\Test
+:: Backup Utility - Backs up the user's files to the network drive
+:: Code by: Kevin Novak
+:: Last Edited: 9/9/2015
+
+@echo off
+
+title Backup Utility
+
+:: define a variable containing a single backspace character
+for /f %%A in ('"prompt $H &echo on &for %%B in (1) do rem"') do set BS=%%A
+
+:: =================================================
+:: Create folder 
+:: =================================================
+echo.
+set flashline=
+set /p flashline=%BS%  Please enter the user's flashline: 
+if "%input%"=="yes" goto itemsend
+set phone=
+set /p phone=%BS%  Please enter the last 4 digits of the user's phone number: 
+if not exist "C:\Users\kevin\Desktop\%flashline%-%phone%" mkdir C:\Users\kevin\Desktop\%flashline%-%phone%
+pause
+exit
