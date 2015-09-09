0d954399773adb78921a938d673d1bbcddcaee03
Kevin Novak <kevinmnovak@yahoo.com>
2015-09-09 11:40:43 -0400

    Prompting for folder notes

--- backup.bat
+++ backup.bat
@@ -17,17 +17,38 @@ echo   The backup utility will back up the user's files to the network drive
 echo.
 set /p var=%BS%  Press Enter to Continue:  
 cls
-goto _create
+goto _prompt
 
 :: =================================================
-:: Create folder 
+:: Prompt for Options
 :: =================================================
-:_create
+:_prompt
 echo.
 set flashline=
 set /p flashline=%BS%  Please enter the user's flashline: 
 set phone=
 set /p phone=%BS%  Please enter the last 4 digits of the user's phone number: 
+set askForNote=
+set /p askForNote=%BS%  Would you like to add an aditional note to the folder name?: 
+if "%askForNote%"=="yes" goto _note
+goto _nonote
+
+:: =================================================
+:: Folder Note
+:: =================================================
+:_note
+cls
+echo.
+echo   Please choose a folder note:
+echo     1 Don't delete
+echo     2 Ask before deleting
+echo     3 Key only
+echo     4 Disk image
+echo     5 [Add your own]
+echo.
+set /p var=%BS%  Press Enter to Continue: 
+
+:_nonote
 if not exist "C:\Users\kevin\Desktop\%flashline%-%phone%" mkdir C:\Users\kevin\Desktop\%flashline%-%phone%
 pause
 exit
